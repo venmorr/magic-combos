793ab6c892c0e599c28b35e4e0004f93f3d3a08e
venmorr <stevenmorrrca@email.com>
2023-09-23 20:46:56 -0400

    add combo index rout

--- controllers/combos.js
+++ controllers/combos.js
@@ -1,1 +1,25 @@
 import { Combo } from '../models/combo.js'
+
+function index(req, res) {
+  Combo.find({})
+  .then(combos => {
+    res.render('combos/index', {
+      combos,
+      title: '🌮'
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
+}
+
+export {
+  index,
+  // create,
+  // show,
+  //// flipTasty,
+  // edit,
+  // update,
+  // deleteCombo as delete,
+}
--- routes/combos.js
+++ routes/combos.js
@@ -3,3 +3,9 @@ import * as combosCtrl from '../controllers/combos.js'
 import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
+
+router.get('/', combosCtrl.index)
+
+export {
+  router
+}
